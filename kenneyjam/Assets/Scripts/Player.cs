6bf176ca00eb3f6db794d13a0d5480da5c99eb81
Neil Warnock <Neil@ecere.com>
2020-08-21 12:04:04 -0400

    Player prefab with movement and placeholder white box sprite

---
+++ kenneyjam/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 1f;
+	public Rigidbody2D rb;
+	
+	
+	private Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		movement.x = Input.GetAxis("Horizontal");
+		movement.y = Input.GetAxis("Vertical");
+				
+		rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+}
