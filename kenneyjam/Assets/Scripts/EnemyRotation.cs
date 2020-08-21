a27209479c3369eec7c1f42adc09ed63dd6d18bd
Anopob <anopob@hotmail.com>
2020-08-21 13:31:15 -0400

    Add Enemy prefab

---
+++ kenneyjam/Assets/Scripts/EnemyRotation.cs
@@ -0,0 +1,21 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotation : MonoBehaviour
+{
+    public AIPath AIPath;
+    private Vector2 _direction;
+
+    private void Update()
+    {
+        FaceVelocity();
+    }
+
+    private void FaceVelocity()
+    {
+        _direction = AIPath.desiredVelocity;
+        transform.right = _direction;
+    }
+}
