f36ec0b67641ec7922a5b327e2af593ecd9bae74
Anopob <anopob@hotmail.com>
2020-08-21 17:58:14 -0400

    Add Constants class

---
+++ kenneyjam/Assets/Scripts/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class Constants
+    {
+        public static int DEFAULT_LAYER = 0;
+        public static int BLOCKERS_LAYER = 8;
+    }
+}
