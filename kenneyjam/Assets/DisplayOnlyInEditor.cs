463b94671591eb25f769598c65842a868981fa66
Anopob <anopob@hotmail.com>
2020-08-21 16:23:26 -0400

    Add EnemyWaypoint prefab

---
+++ kenneyjam/Assets/DisplayOnlyInEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DisplayOnlyInEditor : MonoBehaviour
+{
+    void Start()
+    {
+        if (Application.isPlaying)
+        {
+            var renderer = GetComponent<Renderer>();
+            var color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 0);
+            renderer.material.color = color;
+        }
+    }
+}
