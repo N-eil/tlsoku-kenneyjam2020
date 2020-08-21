f480171544970802153369476dcbef2cb806284d
Neil Warnock <Neil@ecere.com>
2020-08-21 16:21:37 -0400

    Add a level manager that detects win when all runes have been placed on targets

--- kenneyjam/Assets/RuneLocation.cs
+++ kenneyjam/Assets/RuneLocation.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class RuneLocation : MonoBehaviour
 {
     public bool placed = false;
+    
+    private const float RUNE_SCALE = 0.6f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,6 +24,6 @@ public class RuneLocation : MonoBehaviour
     public void PlaceRune(Sprite rune)
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = rune;
-        
+        transform.localScale = new Vector3(RUNE_SCALE, RUNE_SCALE, 1);
     }
 }
---
+++ kenneyjam/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<RuneLocation> runeLocations;
+    private bool active = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(runeLocations);
+        active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!active) return;
+        
+        Debug.Log(runeLocations.Count);
+        foreach (RuneLocation location in runeLocations)
+        {
+            if(location.placed)
+            {
+                Debug.Log("Removing rune locations");
+                runeLocations.Remove(location);
+            }
+        }
+        
+        if (runeLocations.Count == 0)
+        {
+            TriggerWin();
+        }
+    }
+    
+    void TriggerWin()
+    {
+        Debug.Log("You win!");
+        active = false;
+    }
+}
