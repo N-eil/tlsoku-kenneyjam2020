7f28c1df25b315a45441a31d19e0051dc6fa67c0
Anopob <anopob@hotmail.com>
2020-08-21 18:31:50 -0400

    Add Runes scripts

---
+++ kenneyjam/Assets/Scripts/Runes/Rune.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public abstract class Rune : MonoBehaviour
+    {
+        public Sprite Sprite;
+        public abstract string Description { get; }
+    }
+}
---
+++ kenneyjam/Assets/Scripts/Runes/SpeedRune.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Runes
+{
+    public class SpeedRune : Rune
+    {
+        public override string Description { get => "zoom"; }
+    }
+}
