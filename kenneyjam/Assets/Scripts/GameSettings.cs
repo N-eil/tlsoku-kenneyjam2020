f6a206f4b1dc25bf81fc73c83acff5720d83427e
Anopob <anopob@hotmail.com>
2020-08-22 17:34:48 -0400

    Add GameSettings path and class

---
+++ kenneyjam/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings
+{
+    public bool Fullscreen;
+    public int ResolutionIndex;
+    public int TextureQuality;
+    public float MusicVolume;
+}
