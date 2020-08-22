dfa4672b761156e514b1ab2036ad2b5b99022f51
Anopob <anopob@hotmail.com>
2020-08-21 22:26:08 -0400

    Add enemy vision to detect player

--- kenneyjam/Assets/Scripts/Enemy.cs
+++ kenneyjam/Assets/Scripts/Enemy.cs
@@ -1,18 +1,71 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using Pathfinding;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform _targetPlayer;
+    private AIDestinationSetter _aiDestinationSetter;
+
+    [Range(0, 360)]
+    public float ViewAngle = 65;
+
+    private void Start()
     {
-        
+        _aiDestinationSetter = GetComponent<AIDestinationSetter>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (CanPlayerBeSeen())
+        {
+            _aiDestinationSetter.target = _targetPlayer;
+        }
+    }
+
+    private bool CanPlayerBeSeen()
+    {
+        return _targetPlayer && IsPlayerInFieldOfView() && !IsPlayerHiddenByObstacles();
+    }
+
+    private bool IsPlayerInFieldOfView()
+    {
+        Vector2 directionToPlayer = _targetPlayer.position - transform.position;
+        Debug.DrawLine(transform.position, _targetPlayer.position, Color.magenta);
+
+        float angle = Vector3.Angle(transform.position, directionToPlayer);
+
+        return angle < ViewAngle;
+    }
+
+    private bool IsPlayerHiddenByObstacles()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, _targetPlayer.position);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, _targetPlayer.position - transform.position, distanceToPlayer);
+        Debug.DrawRay(transform.position, _targetPlayer.position - transform.position, Color.black);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            // enemies can see through other enemies
+            if (hit.transform.tag == Constants.ENEMY_TAG)
+                continue;
+
+            if (hit.transform.tag != Constants.PLAYER_TAG)
+                return true;
+        }
+        return false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == Constants.PLAYER_TAG)
+            _targetPlayer = collision.gameObject.transform;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == Constants.PLAYER_TAG)
+            _targetPlayer = null;
     }
 }
