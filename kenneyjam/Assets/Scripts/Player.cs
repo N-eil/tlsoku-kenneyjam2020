f3356d884f6c60d9cdcd478e34b72fca4f238261
Anopob <anopob@hotmail.com>
2020-08-23 00:22:47 -0400

    Fix Player indentation

--- kenneyjam/Assets/Scripts/Player.cs
+++ kenneyjam/Assets/Scripts/Player.cs
@@ -6,21 +6,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	public float speed = 25f;
-	public Rigidbody2D rb;
+    public float speed = 25f;
+    public Rigidbody2D rb;
     public int health = 1;
-	public List<Rune> availableRunes;
+    public List<Rune> availableRunes;
     public List<Rune> runeInventory;
     public LevelManager levelManager;
 
     private Transform[] _visionCircles = new Transform[2];
 
     private AudioManager _audioManager;
-	private Vector2 movement;
+    private Vector2 movement;
     private Door nearbyDoor;
     private RuneLocation nearbyRuneLocation;
     private int _selectedRuneIndex = 0;
-    
+
     private int invulnFrameTotal = 75;
     private int invulnFrameCount = 0;
     private int _maxHealth;
@@ -30,18 +30,18 @@ public class Player : MonoBehaviour
         _audioManager = GetComponent<AudioManager>();
         Transform spriteChild = transform.GetChild(0);
         _visionCircles[0] = spriteChild.GetChild(0);
-        _visionCircles[1] = spriteChild.GetChild(1);   
-/*
-        runeInventory.Add(Instantiate(availableRunes[0]));
-        runeInventory.Add(Instantiate(availableRunes[0]));
-        runeInventory.Add(Instantiate(availableRunes[0]));
-        runeInventory.Add(Instantiate(availableRunes[1]));
-        runeInventory.Add(Instantiate(availableRunes[2]));
-*/        
+        _visionCircles[1] = spriteChild.GetChild(1);
+        /*
+                runeInventory.Add(Instantiate(availableRunes[0]));
+                runeInventory.Add(Instantiate(availableRunes[0]));
+                runeInventory.Add(Instantiate(availableRunes[0]));
+                runeInventory.Add(Instantiate(availableRunes[1]));
+                runeInventory.Add(Instantiate(availableRunes[2]));
+        */
         levelManager.hudManager.FillRunes(runeInventory);
 
         Debug.Log(runeInventory.Count);
-        
+
         foreach (Rune rune in runeInventory)
         {
             if (rune is HealthRune)
@@ -54,7 +54,7 @@ public class Player : MonoBehaviour
                 _visionCircles[1].transform.localScale = new Vector3(v.x + Constants.VISION_RUNE_INCREASE, v.y + Constants.VISION_RUNE_INCREASE, v.z);
             }
         }
-        
+
         _maxHealth = health;
         levelManager.hudManager.SetLives(health);
     }
@@ -65,48 +65,48 @@ public class Player : MonoBehaviour
         {
             PerformAction();
         }
-        
+
         if (Input.GetButtonDown("CycleLeft"))
         {
-            SelectRune((_selectedRuneIndex - 1 + runeInventory.Count)  % runeInventory.Count);
+            SelectRune((_selectedRuneIndex - 1 + runeInventory.Count) % runeInventory.Count);
         }
-        
+
         if (Input.GetButtonDown("CycleRight"))
         {
-            SelectRune((_selectedRuneIndex + 1 + runeInventory.Count)  % runeInventory.Count);
+            SelectRune((_selectedRuneIndex + 1 + runeInventory.Count) % runeInventory.Count);
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-		// Movement along both axes
-		movement.x = Input.GetAxis("Horizontal");
-		movement.y = Input.GetAxis("Vertical");
+        // Movement along both axes
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
         float speedModifier = 1;
-        foreach(Rune rune in runeInventory)
+        foreach (Rune rune in runeInventory)
         {
             if (rune is SpeedRune)
                 speedModifier *= 1.2f;
         }
 
-		rb.velocity =  movement * speed * Time.deltaTime * speedModifier;
+        rb.velocity = movement * speed * Time.deltaTime * speedModifier;
         if (rb.velocity != Vector2.zero)
         {
             transform.right = rb.velocity;
         }
-        
+
         if (invulnFrameCount != 0)
         {
             invulnFrameCount--;
         }
-        
+
         if (health <= 0)
         {
             levelManager.PlayerDied();
         }
-	}
-    
+    }
+
     void OnCollisionStay2D(Collision2D collision)
     {
         Enemy enemy = collision.gameObject.GetComponent<Enemy>();
@@ -116,9 +116,9 @@ public class Player : MonoBehaviour
             invulnFrameCount = invulnFrameTotal;
             levelManager.hudManager.SetLives(health);
             _audioManager.PlayDamageSound();
-        }   
+        }
     }
-    
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("collided with trigger");
@@ -129,7 +129,7 @@ public class Player : MonoBehaviour
             nearbyDoor = collidedDoor;
         }
     }
-    
+
     void OnCollisionExit2D(Collision2D collision)
     {
 
@@ -138,9 +138,9 @@ public class Player : MonoBehaviour
         if (collidedDoor && collidedDoor == nearbyDoor)
         {
             nearbyDoor = null;
-        }   
+        }
     }
-    
+
     void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log("collided with trigger");
@@ -156,9 +156,9 @@ public class Player : MonoBehaviour
             nearbyDoor = door;
         }
     }
-    
+
     void OnTriggerExit2D(Collider2D collider)
-    {     
+    {
         //Rune placement collision
         RuneLocation collidedRuneLocation = collider.gameObject.GetComponent<RuneLocation>();
         if (collidedRuneLocation && collidedRuneLocation == nearbyRuneLocation)
@@ -172,18 +172,18 @@ public class Player : MonoBehaviour
         }
     }
 
-	private void PerformAction()
-	{
-		// Interact with doors
-		if (nearbyDoor)
-		{
-			nearbyDoor.ToggleDoor();
+    private void PerformAction()
+    {
+        // Interact with doors
+        if (nearbyDoor)
+        {
+            nearbyDoor.ToggleDoor();
             _audioManager.PlayDoorSound();
-		}
-		
-		// Place runes
-		if (nearbyRuneLocation)
-		{
+        }
+
+        // Place runes
+        if (nearbyRuneLocation)
+        {
             if (!nearbyRuneLocation.placed)
             {
                 nearbyRuneLocation.PlaceRune(runeInventory[_selectedRuneIndex].Sprite);
@@ -207,19 +207,19 @@ public class Player : MonoBehaviour
                 runeInventory.RemoveAt(_selectedRuneIndex);
                 if (_selectedRuneIndex >= runeInventory.Count)
                 {
-                    _selectedRuneIndex = runeInventory.Count -1;
+                    _selectedRuneIndex = runeInventory.Count - 1;
                 }
                 levelManager.hudManager.RemoveRune();
 
                 _audioManager.PlayRunePlacementSound();
             }
         }
-	}
-    
+    }
+
     void SelectRune(int index)
     {
         _selectedRuneIndex = index;
         levelManager.hudManager.SelectRune(index);
     }
-    
+
 }
