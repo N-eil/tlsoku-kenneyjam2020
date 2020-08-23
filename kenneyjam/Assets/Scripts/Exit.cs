6e69668534c8fed0298a7cc7f811bbadea1f7a17
Anopob <anopob@hotmail.com>
2020-08-22 21:20:11 -0400

    Add return to MainMenu on game over

--- kenneyjam/Assets/Scripts/Exit.cs
+++ kenneyjam/Assets/Scripts/Exit.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(sceneName: "MainMenu");
+    }
+
     public void ExitApplication()
     {
         Application.Quit();
