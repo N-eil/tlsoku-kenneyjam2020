5241f61b19d0064b3a55f39ef549c31548f83c75
Neil Warnock <Neil@ecere.com>
2020-08-22 23:20:29 -0400

    Add instructions page from main menu

---
+++ kenneyjam/Assets/Scripts/Instructions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Instructions : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(sceneName: "MainMenu");
+    }
+
+    public void SwitchToInstructionsScene()
+    {
+        SceneManager.LoadScene(sceneName: "Instructions");
+    }
+}
